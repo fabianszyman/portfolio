9a4b55df476e7f57643b8fd689bf72166078a6fe
Fabian Szyman <fabianszyman@gmail.com>
2023-01-08 12:54:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Portfolio Website - Fabian Szymanczyk - using astro.js
---
+++ src/data/info.ts
@@ -0,0 +1,95 @@
+export const info = {
+  baseUrl: "https://fabianszy.vercel.app",
+  name: "Fabian Szymanczyk",
+  jobDescription: "Product Manager",
+  about: `I am Fabian, a Product Manager based in Munich with more than 5 years of working experience in the industry. In my spare time, I am interested in exploring new technologies, particularly in the areas of crypto, programming, and Web3. 
+  
+  While I being in charge of numerous IT-projects covering Front-End, Back-End and Native Apps, I recently developed a strong interesed in Crypto, NFT and Blockchain technology.
+
+  As a Product Manager, I’ve always been interested in staying up to date with the latest and greatest technologies. However, I’ve never had the desire or time to dive deep into coding. That’s why I’ve decided to start this blog called nft-hypebeast.com to document my progress as I learn how to code and explore the world of Crypto, NFTs and Blockchain.
+  
+  
+  `,
+
+  experience: [
+    {
+      name: "InterNative Labs, LLC",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "Apr 2022",
+      endDate: "Present",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+
+    {
+      name: "Coupon Approve (Remote)",
+      location: "Amsterdam, North Holland, Netherlands",
+      startDate: "Nov 2020",
+      endDate: "Apr 2022",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+
+    {
+      name: "Interns Pakistan",
+      location: "Pakistan",
+      startDate: "Oct 2020",
+      endDate: "Nov 2020",
+      description: [
+        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
+        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2019",
+      endDate: "2022",
+      description: [
+        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+      ],
+    },
+    {
+      name: "Govt. Degree Boys College Buffer Zone",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2018",
+      endDate: "2020",
+      description: ["Intermediate, Pre-Engineering"],
+    },
+    {
+      name: "Ghazi Foundation Schools & Colleges",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2016",
+      endDate: "2018",
+      description: ["Matriculation,  Computer Science"],
+    },
+  ],
+
+  socialMedia: {
+    blog: "https://www.nft-hypebeast.com",
+    github: "https://github.com/fabianszyman",
+    email: "mailto:fabianszy@gmail.com",
+    linkedin: "https://www.linkedin.com/in/fabian-szymanczyk-30a801154/",
+  },
+
+  projects: [
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
